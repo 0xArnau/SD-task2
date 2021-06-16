232c0765d0502b140288c0c0a8a98f2a42b45060
arnau <none>
2021-06-16 12:33:48 +0200

    Renamed

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,2 +1,12 @@
+from sentiment_analysis_spanish import sentiment_analysis
+import pandas
+
 if __name__ == "__main__":
   print('Data Preprocessing')
+
+  df = pandas.read_csv('data.csv')
+
+  sentiment = sentiment_analysis.SentimentAnalysisSpanish()
+  df['sentiment_analysis'] = df['text'].apply(lambda i: sentiment.sentiment(i))
+
+  df.to_csv('data_with_sentiment.csv')

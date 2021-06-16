9b0739911c437b2544eb362672d9d05fd126d078
arnau <none>
2021-06-16 15:39:11 +0200

    lithops futures are working now with custom runtime

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import lithops
 from config.config import config
 
 
-#fexec = lithops.FunctionExecutor(config=config,runtime='arppi/sd-lithops-custom-runtime-38:0.1')
+
 def my_function(x):
     return x + 7
 
 
 if __name__ == '__main__':
-    fexec = lithops.FunctionExecutor(config=config)
+    fexec = lithops.FunctionExecutor(config=config,runtime='arppi/sd-lithops-custom-runtime-38:0.1')
+    #fexec = lithops.FunctionExecutor(config=config)
     fexec.call_async(my_function, 3)
     print(fexec.get_result())

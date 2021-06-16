8f53918c28277b2d203c1e372c5ab4f17fce4b6f
arnau <none>
2021-06-16 15:38:33 +0200

    lithops futures are working now

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import lithops
+from config.config import config
 
-#fexec = lithops.FunctionExecutor(runtime='arppi/sd-lithops-custom-runtime:0.1')
-fexec = lithops.ServerlessExecutor()
-def hello(data):
-  return f"Hello {data}"
 
-future = fexec.call_async(hello, 'world')
+#fexec = lithops.FunctionExecutor(config=config,runtime='arppi/sd-lithops-custom-runtime-38:0.1')
+def my_function(x):
+    return x + 7
+
+
+if __name__ == '__main__':
+    fexec = lithops.FunctionExecutor(config=config)
+    fexec.call_async(my_function, 3)
+    print(fexec.get_result())

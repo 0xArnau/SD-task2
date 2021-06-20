bdaee5c2e33cc66f68f952e919b9c05d5fe71b06
arnau <none>
2021-06-20 19:28:53 +0200

    add config.py file

---
+++ config/config.py
@@ -0,0 +1,26 @@
+config = {
+    'lithops': {
+        'storage_bucket': ''
+    },
+    'ibm_cf': {
+        'endpoint': '',
+        'namespace': '',
+        'api_key': '',
+    },
+    'ibm_cos': {
+        'region': '',
+        'endpoint': '',
+        'endpoint_url': '',
+        'api_key': '',
+        'access_key': '',
+        'secret_key': '',
+        'resource_instance_id': ''
+    },
+    'tweepy': {
+        'API_KEY': '',
+        'API_SECRET_KEY': '',
+        'BEARER_TOKEN': '',
+        'ACCESS_TOKEN': '',
+        'ACCESS_SECRET_TOKEN': ''
+    }
+}
